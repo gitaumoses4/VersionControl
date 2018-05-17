068c7602319e19b57ad16b7377a6c84e9c0a5c92
Moses Gitau <gitaumoses4@gmail.com>
2018-05-17 15:22:20 +0300

    Created a module to perform fibonanci sequence

---
+++ fibonanci.py
@@ -0,0 +1,11 @@
+if __name__=='__main__':
+	end = input("Enter a number greater than 2: ")
+	if end > 2:
+		fib = [0,1]
+		for i in range(1,end):
+			fib.append(fib[i]+fib[i-1])
+	
+		print("These are the first {0} fibonanci numbers".format(end))
+		print(fib)
+	else:
+		print("%d is not greater than 2" % (end))
